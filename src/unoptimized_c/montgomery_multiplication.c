4ce037a2bbe38b1cb4a13a666b0a4f72c57b1344
Cailan St Martin <cailan@marinelabs.io>
2018-06-04 22:51:15 -0700

    Create initial structure, makefile and working hello world

---
+++ src/unoptimized_c/montgomery_multiplication.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "montgomery_multiplication.h"
+
+void test()
+{
+  printf("Hello, world!\n");
+}
---
+++ src/unoptimized_c/runner.c
@@ -0,0 +1,6 @@
+#include "montgomery_multiplication.h"
+
+int main()
+{
+  test();
+}
