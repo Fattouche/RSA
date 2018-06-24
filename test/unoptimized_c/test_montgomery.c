06e5bdb19935ad991d0c304df0d43f77fe7c65f4
Cailan St Martin <cailan@marinelabs.io>
2018-06-23 19:19:36 -0700

    Fix some issues to get tests to run

--- test/unoptimized_c/test_montgomery.c
+++ test/unoptimized_c/test_montgomery.c
@@ -3,14 +3,21 @@
 #include <montgomery_multiplication.h>
 #include <uint128_t.h>
 
-void print_128_test_result(uint128_t actual, uint128_t expected, const char* description)
+typedef struct _results {
+  int passed;
+  int failed;
+} results;
+
+void print_128_test_result(uint128_t actual, uint128_t expected, const char* description, results* test_results)
 {
   if (equals_128(actual, expected))
   {
-    printf("%s\n", description);
+    (test_results->passed)++;
+    //printf("%s\n", description);
   }
   else
   {
+    (test_results->failed)++;
     printf("%s failed: ", description);
     printf("Expected: %llu, %llu ", expected.ms_bytes, expected.ls_bytes);
     printf("Actual: %llu, %llu\n", actual.ms_bytes, actual.ls_bytes);
@@ -19,25 +26,30 @@ void print_128_test_result(uint128_t actual, uint128_t expected, const char* des
 
 int main()
 {
-  printf("unsigned long long size: %d", sizeof(unsigned long long));
-  // {  
-  //   uint128_t a_test = {135321511, 348503485};
-  //   uint128_t b_test = {984327520835, 198375328057230};
+  results test_results = {0, 0};
 
-  //   uint128_t a_b_result = add_128(a_test, b_test);
-  //   uint128_t a_b_expected = {984462842346, 198375676560715};
-  //   print_128_test_result(a_b_result, a_b_expected, "Addition test 1");
-  // }
-  // {
-  //   uint128_t a_test = {135321511, 348503485};
-  //   uint128_t b_test = {984327520835, 198375328057230};
+  {  
+    uint128_t a_test = {135321511ULL, 348503485ULL};
+    uint128_t b_test = {984327520835ULL, 198375328057230ULL};
 
-  //   uint128_t a_b_result = add_128(a_test, b_test);
-  //   uint128_t a_b_expected = {984462842346, 198375676560715};
-  //   print_128_test_result(a_b_result, a_b_expected, "Addition test 2");
-  // }
+    uint128_t a_b_result = add_128(a_test, b_test);
+    uint128_t a_b_expected = {984462842346ULL, 198375676560715ULL};
+    print_128_test_result(a_b_result, a_b_expected, "Addition test 1", &test_results);
+  }
+  {
+    uint128_t a_test = {135321511ULL, 348503485ULL};
+    uint128_t b_test = {984327520835ULL, 198375328057230ULL};
 
+    uint128_t a_b_result = add_128(a_test, b_test);
+    uint128_t a_b_expected = {984462842346ULL, 198375676560715ULL};
+    print_128_test_result(a_b_result, a_b_expected, "Addition test 2", &test_results);
+  }
 
+  printf("\n");
+  printf("***** All Tests Run ******\n");
+  printf("Passed: %d\n", test_results.passed);
+  printf("Failed: %d\n", test_results.failed);
+  printf("\n");
 
   return 0;
 }
