44129c93ff627e3a5737c7d1ddb87b256b5552b2
fattouche <alex.fattouche@gmail.com>
2018-07-17 15:36:44 -0700

    Add montgomery_exp

---
+++ src/optimized_c/montgomery_exponentiation.h
@@ -0,0 +1,12 @@
+#ifndef MONTGOMERY_EXPONENTIATION_H
+#define MONTGOMERY_EXPONENTIATION_H
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "uint128_t.h"
+
+uint128_t montgomery_exponentiation();
+
+#endif  // MONTGOMERY_EXPONENTIATION_H
