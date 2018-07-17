b7ac949cc158979ad7d6011e27a6b51c933fc979
CailanStm <cailanstm@shaw.ca>
2018-07-17 14:49:59 -0700

    Use #define DEBUG instead of an int

--- src/unoptimized_c/montgomery_multiplication.c
+++ src/unoptimized_c/montgomery_multiplication.c
@@ -1,5 +1,5 @@
 #include "montgomery_multiplication.h"
-int DEBUG = 0;
+#define DEBUG
 
 // Computes X * Y mod m
 uint128_t montgomery_multiplication(uint128_t x, uint128_t y, uint128_t m) {
@@ -10,11 +10,11 @@ uint128_t montgomery_multiplication(uint128_t x, uint128_t y, uint128_t m) {
   int i, Xi, xANDy, t0;
   int y0 = getBitAtIndex(y, 0);
   int loopLimit = getNumBits(m);
-  // printf("Starting!\n");
-  // printf("y0: %d, looplimit: %d, x: ", y0, loopLimit);
-  if (DEBUG) {
+
+  #ifdef DEBUG
     printf("i  Xi  T(0)+X(i)*Y(0)=n  (T+X(i)*Y+n*M)/2=T\n");
-  }
+  #endif
+  
   for (i = 0; i < loopLimit; i++) {
     t0 = getBitAtIndex(T, 0);
     Xi = getBitAtIndex(x, i);
@@ -23,26 +23,29 @@ uint128_t montgomery_multiplication(uint128_t x, uint128_t y, uint128_t m) {
     XiTimesY = multiply_uint128(Xi, y);
     NTimesM = multiply_uint128(n, m);
     int prev_T = T.ls_bytes;
+    
     //  T = (T + Xi * y + n * m) >> 1;
-    T = bitshift_uint128_right((add_uint128(add_uint128(T, XiTimesY), NTimesM)),
-                               1);
-    if (DEBUG) {
+    T = bitshift_uint128_right(
+      (add_uint128(add_uint128(T, XiTimesY), NTimesM)), 1
+    );
+
+    #ifdef DEBUG
       // currently only works when the uint_128 only uses least significant
       printf(
           "%d  %d    %d + %d*%d = %d      (%d + %d*%llu + %d*%llu)/2 = "
           "%llu\n\n",
           i, Xi, t0, Xi, y0, n, prev_T, Xi, y.ls_bytes, n, m.ls_bytes,
           T.ls_bytes);
-    }
+    #endif
   }
   if (greater_than_or_equal_uint128(T, m)) {
     T = subtract_uint128(T, m);
   }
-  if (DEBUG) {
+  #ifdef DEBUG
     printf("T = ");
     print_uint128(T);
     printf("\n\n");
-  }
+  #endif
 
   return T;
 }
