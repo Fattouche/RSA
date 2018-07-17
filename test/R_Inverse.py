379e0e56b93377aa09c5bd5711f129f7b82de755
fattouche <alex.fattouche@gmail.com>
2018-07-17 12:22:31 -0700

    Add algorithm in python

--- test/R_Inverse.py
+++ test/R_Inverse.py
@@ -40,7 +40,23 @@ def modInverse(a, m):
     return x
 
 
-def calculate(x, y, r_inv, m):
+def algorithm_calculation(x, y, m):
+    T = 0
+    for i in range(m.bit_length()):
+        n = bit_at_index(T, 0) | (bit_at_index(x, i) & bit_at_index(y, 0))
+        T = (T+bit_at_index(x, i)*y+n*m)/2
+    if T >= m:
+        T = T-m
+    return T
+
+
+def bit_at_index(num, index):
+    if int(num) & 1 << index > 0:
+        return 1
+    return 0
+
+
+def exact_calculation(x, y, r_inv, m):
     val = (x*y*r_inv) % m
     return val
 
@@ -59,7 +75,9 @@ m = 93
 
 bit_length = m.bit_length()
 r_inv = modInverse(2**bit_length, m)
-calculation = calculate(x, y, r_inv, m)
+exact = exact_calculation(x, y, r_inv, m)
+algorithm = algorithm_calculation(x, y, m)
 print("Modular multiplicative inverse is", get_hex(r_inv))
 print(
-    "Calculation of\n  {0} (x)\n* {1} (y)\n* {2} (r_inv)\n% {3} (m)\n\n= {4} (T)".format(get_hex(x), get_hex(y), get_hex(r_inv), get_hex(m), get_hex(calculation)))
+    "Calculation of\n  {0} (x)\n* {1} (y)\n* {2} (r_inv)\n% {3} (m)\n\n= {4} (Calculated T)".format(get_hex(x), get_hex(y), get_hex(r_inv), get_hex(m), get_hex(exact)))
+print("  {0} (Algorithm T)".format(get_hex(int(algorithm))))
