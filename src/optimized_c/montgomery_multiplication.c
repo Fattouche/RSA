018306947cc3c695434b9a4c171c64a0693b6abe
Alex Fattouche <alex.fattouche@gmail.com>
2018-07-20 16:57:43 -0700

    Add software pipelining (#5)
    * Add software pipelining

--- src/optimized_c/montgomery_multiplication.c
+++ src/optimized_c/montgomery_multiplication.c
@@ -7,31 +7,41 @@ uint128_t montgomery_multiplication(uint128_t x, uint128_t y, uint128_t m) {
   uint128_t NTimesM;
   int n;
   register int i;
+  register int zero = 0;
   int Xi, xANDy, t0;
-  register int y0 = getBitAtIndex(y, 0);
+  register int y0 = getBitAtIndex(y, zero);
 
   register int m_num_bits = getNumBits(m);
   register int x_num_bits = getNumBits(x);
   register int y_num_bits = getNumBits(y);
 
-  register int num_bits = m_num_bits > x_num_bits ? m_num_bits : x_num_bits;
+  int num_bits = m_num_bits > x_num_bits ? m_num_bits : x_num_bits;
   num_bits = y_num_bits > num_bits ? y_num_bits : num_bits;
 
-  for (i = 0; i < num_bits; i++) {
-    t0 = getBitAtIndex(T, 0);
-    Xi = getBitAtIndex(x, i);
+  t0 = getBitAtIndex(T, zero);
+  Xi = getBitAtIndex(x, 0);
+  xANDy = Xi & y0;
+  n = (t0 ^ xANDy);
+  XiTimesY = multiply_uint128(Xi, y);
+  NTimesM = multiply_uint128(n, m);
+
+  for (i = 0; i < num_bits - 1; i++) {
+    T = bitshift_uint128_right((add_uint128(add_uint128(T, XiTimesY), NTimesM)),
+                               1);
+
+    t0 = getBitAtIndex(T, zero);
+    Xi = getBitAtIndex(x, i + 1);
     xANDy = Xi & y0;
     n = (t0 ^ xANDy);
     XiTimesY = multiply_uint128(Xi, y);
     NTimesM = multiply_uint128(n, m);
-
-    //  T = (T + Xi * y + n * m) >> 1;
-    T = bitshift_uint128_right(
-        (add_uint128(add_uint128(T, XiTimesY), NTimesM)));
   }
+
+  T = bitshift_uint128_right((add_uint128(add_uint128(T, XiTimesY), NTimesM)),
+                             1);
+
   if (greater_than_or_equal_uint128(T, m)) {
     T = subtract_uint128(T, m);
   }
-
   return T;
 }
