4391d11799db21010ad5fc275a0df016153a3c76
fattouche <alex.fattouche@gmail.com>
2018-07-20 13:28:59 -0700

    Add another test

--- src/unoptimized_c/montgomery_exponentiation.c
+++ src/unoptimized_c/montgomery_exponentiation.c
@@ -1,7 +1,8 @@
 #include "montgomery_exponentiation.h"
 
 // Computes x^e mod m
-uint128_t montgomery_exponentiation(uint128_t x, uint128_t e, uint128_t m) {
+uint128_t montgomery_exponentiation(uint128_t x, uint128_t e, uint128_t m,
+                                    uint128_t r_squared_mod_m) {
   int m_num_bits = getNumBits(m);
   int x_num_bits = getNumBits(x);
   int e_num_bits = getNumBits(e);
@@ -9,10 +10,9 @@ uint128_t montgomery_exponentiation(uint128_t x, uint128_t e, uint128_t m) {
   int loop_limit = m_num_bits > x_num_bits ? m_num_bits : x_num_bits;
   loop_limit = e_num_bits > loop_limit ? e_num_bits : loop_limit;
 
-  uint128_t r_squared = get_r_squared_uint128(m);
   uint128_t one = {0ULL, 1ULL};
-  uint128_t z = montgomery_multiplication(one, r_squared, m);
-  uint128_t p = montgomery_multiplication(x, r_squared, m);
+  uint128_t z = montgomery_multiplication(one, r_squared_mod_m, m);
+  uint128_t p = montgomery_multiplication(x, r_squared_mod_m, m);
   int i;
   for (i = 0; i < loop_limit; i++) {
     if (getBitAtIndex(e, i)) {
@@ -22,41 +22,4 @@ uint128_t montgomery_exponentiation(uint128_t x, uint128_t e, uint128_t m) {
   }
   z = montgomery_multiplication(one, z, m);
   return z;
-}
-
-// Returns r^2 % modular
-// Precondition: The number of significant bits in modular must be <= 128
-uint128_t get_r_squared_uint128(uint128_t modular) {
-  /*
-   * (1 << (2 * num_bits(modular))) % modular
-   */
-  // int m = getNumBits(modular);
-
-  // printf("modular: ");
-  // print_uint128(modular);
-  // printf("\n");
-
-  // printf("num bits: %d\n", m);
-
-  // uint128_t r_128 = {0ULL, (unsigned long long) 1 << m};
-  
-  // printf("r_128: ");
-  // print_uint128(r_128);
-  // printf("\n");
-
-  // uint128_t one = {0, 1};
-
-  // uint128_t r_modular = montgomery_multiplication(one, r_128, modular, m);
-
-  // printf("r_modular: ");
-  // print_uint128(r_modular);
-  // printf("\n");
-
-  // unsigned long long r_squared = 1 << (2 * ((unsigned long long) num_bits_in_modular));
-
-  // uint128_t r_squared_128 = {0ULL, r_squared};
-
-  // return r_squared_128;
-  uint128_t result = {0ULL, 1179ULL};
-  return result;
 }
--- test/test_montgomery_exponentiation.c
+++ test/test_montgomery_exponentiation.c
@@ -8,30 +8,34 @@
 int main() {
   results test_results = {0, 0};
   {
-    uint128_t a_test = {0ULL, 123ULL};
-    uint128_t e_test = {0ULL, 17ULL};
+    uint128_t a_test = {0ULL, 855ULL};
+    uint128_t e_test = {0ULL, 2753ULL};
     uint128_t m_test = {0ULL, 3233ULL};
-
-    uint128_t mont_result = montgomery_exponentiation(a_test, e_test, m_test);
-    uint128_t mont_expected = {0LL, 855ULL};
+    uint128_t r_squared_mod_m = {0ULL, 1179ULL};
+    uint128_t mont_result =
+        montgomery_exponentiation(a_test, e_test, m_test, r_squared_mod_m);
+    uint128_t mont_expected = {0LL, 123ULL};
 
     print_uint128_test_result(mont_result, mont_expected,
                               "Low Montgomery exponentiation test",
                               &test_results);
   }
 
-  //   {
-  //     uint128_t a_test = {0x0ULL, 0xc6beeULL};
-  //     uint128_t b_test = {0x0ULL, 0xf665fULL};
-  //     uint128_t m_test = {0x0ULL, 0xfb109ULL};
+  {
+    uint128_t a_test = {0xc6beec7602f3b06ULL, 0x5bb2ac322c1f95e1ULL};
+    uint128_t e_test = {0xf665ffaa6dd4204ULL, 0xb29c6ad048eb9fb1ULL};
+    uint128_t m_test = {0xfb10458be6c5468ULL, 0xe0b603ea54d107edULL};
 
-  //     uint128_t mont_result = montgomery_exponentiation(a_test, b_test,
-  //     m_test); uint128_t mont_expected = {0LL, 0xbfe74ULL};
+    uint128_t r_squared_mod_m = {0xE7D6493C934B9DFULL, 0x0D35F285B481ED8EULL};
 
-  //     print_uint128_test_result(mont_result, mont_expected,
-  //                               "High Montgomery exponentiation test",
-  //                               &test_results);
-  //   }
+    uint128_t mont_result =
+        montgomery_exponentiation(a_test, e_test, m_test, r_squared_mod_m);
+    uint128_t mont_expected = {0x138F051C1385E56ULL, 0x2831894B321357F5ULL};
+
+    print_uint128_test_result(mont_result, mont_expected,
+                              "High Montgomery exponentiation test",
+                              &test_results);
+  }
 
   printf("\n");
   printf("***** All Montgomery Exponentiation Tests Run ******\n");
